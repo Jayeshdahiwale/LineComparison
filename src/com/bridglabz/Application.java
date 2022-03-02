7cf2abbf720dfb1991ab66ea411882088238c9d4
Jayeshdahiwale <jayjdahivale@gmail.com>
2022-03-02 15:46:13 +0530

    Added

---
+++ src/com/bridglabz/Application.java
@@ -0,0 +1,11 @@
+package com.bridglabz;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the line comparison program");
+
+	}
+
+}
