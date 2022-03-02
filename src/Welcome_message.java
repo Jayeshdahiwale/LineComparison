27f8fa01702ddf58aa8cdff7e0405de1297479cb
Jayeshdahiwale <46401270+Jayeshdahiwale@users.noreply.github.com>
2022-03-02 14:27:21 +0530

    Delete src directory

--- src/Welcome_message.java
+++
@@ -1,9 +0,0 @@
-public class Welcome_message {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          System.out.println("Welcome to Line Comparison computation program");
-	}
-
-}
-
